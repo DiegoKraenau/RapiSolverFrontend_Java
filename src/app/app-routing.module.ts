2ee000fe7dc09f38f7bca450bd5d6471d0c5feec
Sebastian242811 <hernandesebastian0@gmail.com>
2021-06-22 00:31:43 -0500

    adding and connecting main stream

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ServicesComponent} from "./components/service/services/services.component";
+import {LoginComponent} from "./components/log/login/login.component";
+import {AboutComponent} from "./components/log/about/about.component";
+import {ServiceAddComponent} from "./components/service/service-add/service-add.component";
+import {ProfileViewComponent} from "./components/profile/profile-view/profile-view.component";
+import {LogupComponent} from "./components/log/logup/logup.component";
 
 const routes: Routes = [
-  {path:'services',component:ServicesComponent}
+  {path:'',component:LoginComponent},
+  {path:'register',component:LogupComponent},
+  {path:'about',component:AboutComponent},
+  {path:'services',component:ServicesComponent},
+  {path:'service_add',component:ServiceAddComponent},
+  {path:'profile',component:ProfileViewComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,33 @@ import { AppComponent } from './app.component';
 import { ServicesComponent } from './components/service/services/services.component';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { LoginComponent } from './components/log/login/login.component';
+import { AboutComponent } from './components/log/about/about.component';
+import { ProfileViewComponent } from './components/profile/profile-view/profile-view.component';
+import { ProfileEditComponent } from './components/profile/profile-edit/profile-edit.component';
+import { ServiceAddComponent } from './components/service/service-add/service-add.component';
+import { LogupComponent } from './components/log/logup/logup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceEditComponent } from './components/service/service-edit/service-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServicesComponent
+    ServicesComponent,
+    LoginComponent,
+    AboutComponent,
+    ProfileViewComponent,
+    ProfileEditComponent,
+    ServiceAddComponent,
+    LogupComponent,
+    ServiceEditComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
